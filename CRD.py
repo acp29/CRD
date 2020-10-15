4e84928684578590e352b0094382f6c839958ea1
Andrew Penn <A.C.Penn@sussex.ac.uk>
2020-10-15 09:12:41 +0100

    Update CRD.py

--- CRD.py
+++ CRD.py
@@ -504,7 +504,7 @@ while True:
             browser.quit()
             quit() 
         # 30 second warning
-        # Message box will timeout in 30 seconds
+        # Message box will timeout in just under 30 seconds
         if (l-t) < 30:
             win32gui.ShowWindow(cmd_handle,win32con.SW_MAXIMIZE)
             win32gui.MoveWindow(cmd_handle,0,0,1000,600,True)
@@ -512,8 +512,7 @@ while True:
             countdown.start()
             tle = "Chrome Remote Desktop Session Alert"
             msg = "This remote desktop session will terminate within 30 seconds.\nPlease save and close your work."
-            ctypes.windll.user32.MessageBoxTimeoutW(0, msg, tle, 0x42030,0, 30000)
-            quit()
+            ctypes.windll.user32.MessageBoxTimeoutW(0, msg, tle, 0x42030,0, 29000)
         # Check if share_bar is empty, if it is then quit
         if share_bar:
             pass
